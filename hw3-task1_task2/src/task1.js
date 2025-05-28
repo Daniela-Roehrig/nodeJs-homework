feda030eb90af22e5aee4502150cbe0e8f4fd060
Daniela <roehrig.87.daniela@gmx.de>
2025-05-28 21:30:50 +0200

    hw3

--- hw3-task1_task2/src/task1.js
+++ hw3-task1_task2/src/task1.js
@@ -1,20 +1,20 @@
 import * as fs from "node:fs/promises";
+import path from "node:path";
 
-const folderName = './src/myFolder';
+const folderName = path.resolve("src", "myFolder");
 
-// create:
+// create Folder
 try {
     await fs.mkdir(folderName);
-    console.log('Folder created.');
+    console.log("Folder created.");
 } catch (err) {
-    console.log('Folder already exists!')
+    console.log("Folder already exists!");
 }
 
-// delete:
-/* try {
+// delete Folder
+try {
     await fs.rmdir(folderName);
-    console.log('Folder deleted.');
+    console.log("Folder deleted.");
 } catch (err) {
-    console.error('Error deleting folder:', err.message);
+    console.error("Error deleting folder:", err.message);
 }
- */
--- hw3-task1_task2/src/task2.js
+++ hw3-task1_task2/src/task2.js
@@ -1,21 +1,23 @@
-import * as fs from "node:fs/promises";
-
-const fileName = './src/info.txt';
-const content = 'Node.js is awesome!';
+import fs from "node:fs";
+import path from "node:path";
 
 
+const fileName = path.resolve("src", "info.txt");
+const text = "Node.js is awesome!";
 
-fs.writeFile(fileName, content, (err) => {
+// write file
+fs.writeFile(fileName, text, (err) => {
     if (err) {
-        console.error('Error writing file:', err.message);
+        console.error("Error writing file:", err.message);
     } else {
-        console.log('File successfully written.');
+        console.log("File successfully written.");
 
-        fs.readFile(fileName, 'utf-8', (err, data) => {
+        // read file
+        fs.readFile(fileName, "utf-8", (err, data) => {
             if (err) {
-                console.error('Error reading file:', err.message);
+                console.error("Error reading file:", err.message);
             } else {
-                console.log('File content:');
+                console.log("File text:");
                 console.log(data);
             }
         });
